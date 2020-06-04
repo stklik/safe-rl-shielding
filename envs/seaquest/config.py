e88dbbc387a3487621427557afdc73dbf25673bf
Stefan Klikovits <stklik@users.noreply.github.com>
2020-06-04 23:40:48 +0900

    Update config.py

--- envs/seaquest/config.py
+++ envs/seaquest/config.py
@@ -66,14 +66,27 @@ def get_config(FLAGS):
   elif FLAGS.model == 'm2':
     config = M2
 
-  for k, v in FLAGS.__dict__['__flags'].items():
-    if k == 'gpu':
-      if v == False:
+# FIX for new TF: https://github.com/devsisters/DQN-tensorflow/issues/50
+
+#   for k, v in FLAGS.__dict__['__flags'].items():
+#     if k == 'gpu':
+#       if v == False:
+#         config.cnn_format = 'NHWC'
+#       else:
+#         config.cnn_format = 'NCHW'
+
+#     if hasattr(config, k):
+#       setattr(config, k, v)
+
+  for k in FLAGS.__dict__['__wrapped']:
+    if k == 'use_gpu':
+      if not FLAGS.__getattr__(k):
         config.cnn_format = 'NHWC'
       else:
         config.cnn_format = 'NCHW'
 
-    if hasattr(config, k):
-      setattr(config, k, v)
+  if hasattr(config, k):
+    setattr(config, k, FLAGS.__getattr__(k))
+
 
   return config
